5969878f9fe790496fc2cf665d161bd667a24754
HiboMan <hiboman@proton.me>
2026-02-21 13:56:29 +0100

    Set DEBUG to False and update ALLOWED_CIDR_NETS

--- admin_panel/admin_panel/settings/local.py
+++ admin_panel/admin_panel/settings/local.py
@@ -1,13 +1,9 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 SECRET_KEY = "insecure"
 INSTALLED_APPS.append("achievement")
 INSTALLED_APPS.append("craftings")
 
 # Only allow connections from local IPs
-ALLOWED_CIDR_NETS = [
-    "10.0.0.0/8",
-    "172.16.0.0/12",
-    "192.168.0.0/16",
-]
+ALLOWED_CIDR_NETS = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
